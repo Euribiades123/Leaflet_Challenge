bf5002671e36b93540ccd1298d1fef3bc23b6633
Euribiades123 <127801942+Euribiades123@users.noreply.github.com>
2023-08-05 10:26:45 -0400

    editing logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -8,7 +8,7 @@ d3.json(url).then(data => {
 function createMap(earthquakeData) {
   const map = L.map('map').setView([0, 0], 2); // Set initial view
 
-  // Add tile layer (e.g., OpenStreetMap)
+  // Add tile layer 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
